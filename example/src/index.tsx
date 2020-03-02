92d1d9665b50af6777ca6b2331ff6868f02509b1
Mikhail <moskalev.mikhail@gmail.com>
2020-03-02 23:07:27 +0800

    increase example

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -1,7 +1,39 @@
 import * as ReactDOM from 'react-dom';
 import * as React from "react";
-import { App } from "./App";
+import { useEffect, useState } from "react";
+import { useVoiceRecorder } from "use-voice-recorder";
 
+export const App: React.FC = () => {
+  const [records, updateRecords] = useState([]);
+  const {isRecording, stop, start} = useVoiceRecorder((data) => {
+    updateRecords([...records, window.URL.createObjectURL(data)]);
+  });
+
+  return (
+    <div className={'container'}>
+      <div className={'hint'}>
+        Just hold the button and speak.
+      </div>
+      <div>
+        <button className={`btn ${isRecording ? 'active' : ''}`}
+                onMouseDown={start}
+                onMouseUp={stop}
+                onTouchStart={start}
+                onTouchEnd={stop}>🎙</button>
+
+        <h3 className={'onair'}>On air: {isRecording ? 'on' : 'off'}</h3>
+      </div>
+      <div>
+        <h1>Records:</h1>
+        {records.map((data, idx) => (
+          <div key={idx}>
+            <audio src={data} controls preload={'metadata'} />
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+};
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
